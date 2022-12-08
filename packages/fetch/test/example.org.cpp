0cd194a8e8586157e332c23dce04afa318f49ca7
Jacob Hummer <jcbhmr@outlook.com>
2022-12-08 18:24:40 +0000

    Improve test output

--- packages/fetch/test/example.org.cpp
+++ packages/fetch/test/example.org.cpp
@@ -3,13 +3,15 @@
 
 int main() {
   auto response = fetch::fetch("https://example.org").get();
-  std::cout << response.url() << "\n";
-  std::cout << response.status() << "\n";
+  std::cout << "URL: " << response.url() << "\n";
+  std::cout << "Status:" << response.status() << "\n";
+  std::cout << "Headers:\n";
   for (auto& [name, value] : response.headers().m_headers) {
-    std::cout << name << ": " << value << "\n";
+    std::cout << "  " << name << ": " << value << "\n";
   }
   std::cout << "Explicitly get Content-Type: "
             << response.headers().get("Content-Type").value_or("<null>")
-            << "\n\n";
+            << "\n";
+  std::cout << "Body:\n";
   std::cout << response.text().get() << "\n";
 }
