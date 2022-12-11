19c939fc8f502ce9390f01d27cfa8a39a9387106
Jacob Hummer <jcbhmr@outlook.com>
2022-12-11 22:54:08 +0000

    Better GIF

--- test/e2e.sh
+++ test/e2e.sh
@@ -3,5 +3,5 @@
 tools/gen-repo-index.sh
 xmake
 mkdir -vp test/__snapshot__
-asciinema rec --overwrite -c 'xmake r' -i 5 --cols 70 --rows 18 test/__snapshot__/e2e.json
+asciinema rec --overwrite -c 'xmake r' -i 5 --cols 70 --rows 24 test/__snapshot__/e2e.json
 agg test/__snapshot__/e2e.json test/__snapshot__/e2e.gif
